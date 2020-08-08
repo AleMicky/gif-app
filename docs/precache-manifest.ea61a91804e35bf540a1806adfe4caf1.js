f5a7ce6659d698815d9200bb55d736f17d90f08f
Ale <miguel.ale19934@gmail.com>
2020-08-08 02:58:07 -0400

    docs

---
+++ docs/precache-manifest.ea61a91804e35bf540a1806adfe4caf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736942521a0e1ee445f72709bfbd2977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e0b0334a5fd90ba79c5",
+    "url": "/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "a105211e0a116ebf2a00",
+    "url": "/static/css/main.1955b768.chunk.css"
+  },
+  {
+    "revision": "4e0b0334a5fd90ba79c5",
+    "url": "/static/js/2.f9c03d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9c03d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a105211e0a116ebf2a00",
+    "url": "/static/js/main.3ab7473d.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
